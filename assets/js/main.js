0732560b20167019c49bbf8bcb80cca9cdb25b85
t0g3pii aka Nico <49153387+t0g3pii@users.noreply.github.com>
2021-02-14 14:14:27 +0100

    disabled keybuttons

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+document.onkeydown = function (event)
+{
+     event = (event || window.event);
+     if (event.keyCode == 123 || 
+     event.keyCode == 123 || 
+     event.keyCode == 122 || 
+     event.keyCode == 121 || 
+     event.keyCode == 120 || 
+     event.keyCode == 119 || 
+     event.keyCode == 118 || 
+     event.keyCode == 117 || 
+     event.keyCode == 116 || 
+     event.keyCode == 115 || 
+     event.keyCode == 114 || 
+     event.keyCode == 113 || 
+     event.keyCode == 112 || 
+     event.keyCode == 36 || 
+     event.keyCode == 18|| 
+     event.keyCode == 17)
+     {
+           return false;
+     }
+}
