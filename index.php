6505e3fc101bb6ae407d9aa892b949588cc4e97c
t0g3pii aka Nico <49153387+t0g3pii@users.noreply.github.com>
2021-02-14 11:30:28 +0100

    removed pw

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
     <form >
 		<label for="username">Username:</label>
 		<input name="username">
-		<label for="password">Passwort:</label>
-		<input type="password" name="password" />
 		<button type="submit" formaction="/login.php">Anmelden</button>
 		<button type="submit" formaction="/register.php">Registrieren</button>
 	</form>
