6ad12cb1659a7e0f8fc8dd1340366a610f335680
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 12:42:12 +0100

    Update db.inc.php

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -4,5 +4,5 @@ define('DB_NAME', 'ni128208_2sql8');
 define('DB_USERNAME', 'ni128208_2sql8');
 define('DB_PASSWORD', 'Passwort');
 
-$pdo = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.'', $DB_USERNAME, $DB_PASSWORD);
+$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.'', DB_USERNAME, DB_PASSWORD);
 ?>
