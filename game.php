1b5bc6d9a616c4938d2f043e1f3b40b110f8760e
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 14:31:52 +0100

    Freebie

--- game.php
+++ game.php
@@ -1,11 +1,37 @@
+<?php
+session_start();
+if (!isset($_SESSION["UUID"])) {
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $extra = 'index.php';
+    header("Location: http://$host$uri/$extra");
+    session_destroy();
+    exit();
+}
+require("inc/db.inc.php");
+
+$st = $pdo->prepare( "SELECT username, credits, lastFree FROM `user` WHERE `UUID` = ?" );
+$st->execute(array($_SESSION["UUID"]));
+$data = $st->fetch();
+echo json_encode($data);
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <script>
             function update() {
-                $.post("gameserver.php",{"action":"getData"} ,(data)=>{
-                    console.log(data);
+                $.post("gameserver.php",{"action":"getData"} ,(rawdata)=>{
+                    let data = JSON.parse(rawdata);
+                    $("#game_username").val(data["username"]);
+                    $("#game_value").val(data["credits"]);
+                });
+            }
+
+            function getFree() {
+                $.post("gameserver.php",{"action":"getFree"} ,()=>{
+                    update();
                 });
             }
         </script>
@@ -20,22 +46,11 @@
         <button>Einsatz +</button>
         <button>Einsatz -</button>
         <button>Spielen</button>
-        <button>Freebee Kepie</button>
+        <button onclick="getFree();">Freebee Kepie</button>
         <button>Ausloggen</button>
     </body>
 </html>
 
 
 <!-- echo $_SESSION["UUID"]; -->
-<!-- <?php
-session_start();
-if (!isset($_SESSION["UUID"])) {
-    $host  = $_SERVER['HTTP_HOST'];
-    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-    $extra = 'index.php';
-    header("Location: http://$host$uri/$extra");
-    session_destroy();
-    exit();
-}
-require("inc/db.inc.php");
-?> -->
+ -->
