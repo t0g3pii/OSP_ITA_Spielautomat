6d9c2f4e9ba4fca7df47b44830d62bdac3082c45
t0g3pii aka Nico <49153387+t0g3pii@users.noreply.github.com>
2021-02-14 11:48:11 +0100

    css

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Startseite - Login</title>
-        <link rel="stylesheet" type="text/css" href="assets/css/main.css" media="screen"/>
-        <script src="assets/js/main.js" async></script>
-	</head>
-    <form >
-		<label for="username">Username:</label>
-		<input name="username">
-		<button type="submit" formaction="/login.php">Anmelden</button>
-		<button type="submit" formaction="/register.php">Registrieren</button>
-	</form>
+
+<head>
+    <title>Startseite - Login</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css" media="screen" />
+    <script src="assets/js/main.js" async></script>
+</head>
+<div id="login" class="modal">
+    <form class="modal-content">
+        <div class="container">
+            <label for="username">Username:</label>
+            <input type="text" placeholder="Benutzername" name="username" required>
+            <button type="submit" style="background-color: green;" formaction="/login.php">Anmelden</button>
+            <button type="submit" style="background-color: red;" formaction="/register.php">Registrieren</button>
+        </div>
+    </form>
+</div>
+
 </html>
