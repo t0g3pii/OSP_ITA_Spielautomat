34511926895c335607191816ca87065272e8f9bd
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 13:36:54 +0100

    Update login.php

--- login.php
+++ login.php
@@ -4,17 +4,17 @@
     $st->execute(array($_GET["username"], $_GET["pin"]));
 
     if ($st->rowCount() == 0) {
-        $host  = $_SERVER['HTTP_HOST'];
-        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-        $extra = 'index.php?error=login';
+        $host  = $_SERVER["HTTP_HOST"];
+        $uri   = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+        $extra = "index.php?error=login";
         header("Location: http://$host$uri/$extra");
     } else {
         session_start();
         $user = $st->fetch();
-        $_SESSION["UUID"] = $user->UUID;
-        $host  = $_SERVER['HTTP_HOST'];
-        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-        $extra = 'game.php';
+        $_SESSION["UUID"] = $user["UUID"];
+        $host  = $_SERVER["HTTP_HOST"];
+        $uri   = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+        $extra = "game.php";
         header("Location: http://$host$uri/$extra");
     }
 ?>
