ba27bc00812f1403727cf8cb6b84e479b34dead4
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 14:13:10 +0100

    Create gameserver.php

---
+++ gameserver.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    if (!isset($_SESSION["UUID"])) {
+        exit("ERROR! Bitte erneut anmelden!");
+    }
+
+    if ($_POST["action"]=="getData") {
+        require("inc/db.inc.php");
+        $st = $pdo->prepare( "SELECT username, credits, lastFree FROM `user` WHERE `UUID` = ?" );
+        $st->execute(array($_SESSION["UUID"]));
+        $data = $st->fetch();
+        echo json_encode(array("username"=>$data["username"], "credits"=>$data["credits"], "lastFree"=>$data["lastFree"]));
+    }
+?>
