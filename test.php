73023f3eb303300b197a65594cffa0aa262099a6
t0g3pii aka Nico <49153387+t0g3pii@users.noreply.github.com>
2021-02-14 15:06:33 +0100

    Update test.php

--- test.php
+++ test.php
@@ -12,6 +12,17 @@ $dateN = new DateTime();
 echo $date->format('Y-m-d H:i:s') ."</br>";
 echo $dateN->format('Y-m-d H:i:s') ."</br>";
 
-echo $date->diff($dateN)->m.' minutes<br>';
+$since_start = $dateN->diff($date));
+echo $since_start->days.' days total<br>';
+echo $since_start->y.' years<br>';
+echo $since_start->m.' months<br>';
+echo $since_start->d.' days<br>';
+echo $since_start->h.' hours<br>';
+echo $since_start->i.' minutes<br>';
+echo $since_start->s.' seconds<br><br>';
 
+$minutes = $since_start->days * 24 * 60;
+$minutes += $since_start->h * 60;
+$minutes += $since_start->i;
+echo $minutes.' minutes';
 ?>
