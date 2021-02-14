b3d89420311bce74eed4c2898a0d21f87d9fcfe9
t0g3pii aka Nico <49153387+t0g3pii@users.noreply.github.com>
2021-02-14 16:15:41 +0100

    Added head

--- game.php
+++ game.php
@@ -10,82 +10,100 @@ if (!isset($_SESSION["UUID"])) {
 }
 require("inc/db.inc.php");
 
-$st = $pdo->prepare( "SELECT username, credits, lastFree FROM `user` WHERE `UUID` = ?" );
+$st = $pdo->prepare("SELECT username, credits, lastFree FROM `user` WHERE `UUID` = ?");
 $st->execute(array($_SESSION["UUID"]));
 $data = $st->fetch();
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-        <script>
-            var wait = false;
-            function update() {
-                $.post("gameserver.php",{"action":"getData"} ,(rawdata)=>{
-                    let data = JSON.parse(rawdata);
-                    $("#game_value").val(data["credits"]);
-                });
-            }
 
-            function getFree() {
-                $.post("gameserver.php",{"action":"getFree"} ,()=>{
-                    update();
-                });
-            }
+<head>
+    <title>Slot Machine - Game</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js" async></script>
+    <script>
+        var wait = false;
 
-            function logout() {
-                $.post("gameserver.php",{"action":"logout"} ,()=>{
-                    window.open("index.php","_self")
-                });
-            }
+        function update() {
+            $.post("gameserver.php", {
+                "action": "getData"
+            }, (rawdata) => {
+                let data = JSON.parse(rawdata);
+                $("#game_value").val(data["credits"]);
+            });
+        }
 
-            function stakeAdd(x) {
-                let oldStake = $("#game_stake").val();
-                let newStake = Number(oldStake) + x;
-                $("#game_stake").val( newStake );
-            }
+        function getFree() {
+            $.post("gameserver.php", {
+                "action": "getFree"
+            }, () => {
+                update();
+            });
+        }
 
-            function stakeRem(x) {
-                let oldStake = $("#game_stake").val();
-                let newStake = oldStake - x;
-                $("#game_stake").val( newStake );
-            }
+        function logout() {
+            $.post("gameserver.php", {
+                "action": "logout"
+            }, () => {
+                window.open("index.php", "_self")
+            });
+        }
 
-            function play() {
-                if (wait) {
-                    alert("Gedulde dich du Sackratte!");
-                    return
-                }
-                wait = true;
-                setTimeout(() => {
-                    wait = false;
-                }, 3000);
-                let stake = Number($("#game_stake").val());
-                $.post("gameserver.php",{"action":"play", "stake": stake} ,(rawData)=>{
-                    console.log(rawData);
-                    update();
-                    let data = JSON.parse(rawData); // roleOne, roleTwo, roleThree, WinAmount
-                    document.getElementById("game_slot1").value = data[0];
-                    document.getElementById("game_slot2").value = data[1];
-                    document.getElementById("game_slot3").value = data[2];
-                    document.getElementById("game_win").value = data[3];
-                });
+        function stakeAdd(x) {
+            let oldStake = $("#game_stake").val();
+            let newStake = Number(oldStake) + x;
+            $("#game_stake").val(newStake);
+        }
+
+        function stakeRem(x) {
+            let oldStake = $("#game_stake").val();
+            let newStake = oldStake - x;
+            $("#game_stake").val(newStake);
+        }
+
+        function play() {
+            if (wait) {
+                alert("Gedulde dich du Sackratte!");
+                return
             }
-        </script>
-    </head>
-    <body>
-        <input id="game_username" value="<?php echo $data["username"]; ?>" disabled>
-        <input id="game_value" value="<?php echo $data["credits"]; ?>" disabled>
-        <input id="game_stake" value="5" type="number" disabled>;
-        <input id="game_slot1" disabled>
-        <input id="game_slot2" disabled>
-        <input id="game_slot3" disabled>
-        <input id="game_win" disabled>
-        <button onclick="stakeAdd(1);">Einsatz +</button>
-        <button onclick="stakeRem(1);">Einsatz -</button>
-        <button onclick="play();">Spielen</button>
-        <button onclick="getFree();" <?php echo "disabled"; ?>>Freebee Kepie</button>
-        <button onclick="logout();">Ausloggen</button>
-    </body>
+            wait = true;
+            setTimeout(() => {
+                wait = false;
+            }, 3000);
+            let stake = Number($("#game_stake").val());
+            $.post("gameserver.php", {
+                "action": "play",
+                "stake": stake
+            }, (rawData) => {
+                console.log(rawData);
+                update();
+                let data = JSON.parse(rawData); // roleOne, roleTwo, roleThree, WinAmount
+                document.getElementById("game_slot1").value = data[0];
+                document.getElementById("game_slot2").value = data[1];
+                document.getElementById("game_slot3").value = data[2];
+                document.getElementById("game_win").value = data[3];
+            });
+        }
+    </script>
+</head>
+
+<body>
+    <input id="game_username" value="<?php echo $data["username"]; ?>" disabled>
+    <input id="game_value" value="<?php echo $data["credits"]; ?>" disabled>
+    <input id="game_stake" value="5" type="number" disabled>;
+    <input id="game_slot1" disabled>
+    <input id="game_slot2" disabled>
+    <input id="game_slot3" disabled>
+    <input id="game_win" disabled>
+    <button onclick="stakeAdd(1);">Einsatz +</button>
+    <button onclick="stakeRem(1);">Einsatz -</button>
+    <button onclick="play();">Spielen</button>
+    <button onclick="getFree();" <?php echo "disabled"; ?>>Freebee Kepie</button>
+    <button onclick="logout();">Ausloggen</button>
+</body>
+
 </html>
