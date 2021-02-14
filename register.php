47b6ac1432f9c254a9adde75c7057629a64e3812
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 12:46:22 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
     require("inc/db.inc.php");
     $result = $pdo->query( "INSERT INTO `user` (`username`) VALUES ('".$_GET['username']."');" );
-    print_r( $result );
+    print_r( $pdo->lastInsertId );
 ?>
