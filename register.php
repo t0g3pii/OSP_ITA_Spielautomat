26b66c4d4e67890cd974cfc10c8fec1216b542d0
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 13:26:16 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,17 +1,18 @@
 <?php
     require("inc/db.inc.php");
-    $st = $pdo->prepare( "INSERT INTO `user` (`username`) VALUES ('".$_GET['username']."');" );
+    $st = $pdo->prepare( "INSERT INTO `user` (`username`, `pin`) VALUES (?, ?);" );
+    $st->execute(array($_GET["username"], $_GET["pin"]))
     if (!$st->execute()) {
         $host  = $_SERVER['HTTP_HOST'];
         $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-        $extra = 'index.php?error=username';
+        $extra = 'index.php?error=register';
         header("Location: http://$host$uri/$extra");
     } else {
-        session_start();
-        $_SESSION["UUID"] = $st->lastInsertId;
-        $host  = $_SERVER['HTTP_HOST'];
-        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-        $extra = 'game.php';
-        header("Location: http://$host$uri/$extra");
+        // session_start();
+        // $_SESSION["UUID"] = $st->lastInsertId;
+        // $host  = $_SERVER['HTTP_HOST'];
+        // $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        // $extra = 'game.php';
+        // header("Location: http://$host$uri/$extra");
     }
 ?>
