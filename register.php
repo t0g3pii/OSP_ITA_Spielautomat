fa6172792ab91e4adf08977d6c160dce5e5ff4b4
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 13:32:11 +0100

    Update register.php

--- register.php
+++ register.php
@@ -8,11 +8,11 @@
         $extra = 'index.php?error=register';
         header("Location: http://$host$uri/$extra");
     } else {
-        // session_start();
-        // $_SESSION["UUID"] = $st->lastInsertId;
-        // $host  = $_SERVER['HTTP_HOST'];
-        // $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-        // $extra = 'game.php';
-        // header("Location: http://$host$uri/$extra");
+        session_start();
+        $_SESSION["UUID"] = $st->lastInsertId;
+        $host  = $_SERVER['HTTP_HOST'];
+        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        $extra = 'game.php';
+        header("Location: http://$host$uri/$extra");
     }
 ?>
