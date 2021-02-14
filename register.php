e243437409a843add77748a4dad34d5c32331dca
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 12:38:09 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+    error_reporting( E_ALL );
     require("inc/db.inc.php");
     $result = $pdo->query( "INSERT INTO `user` (`username`) VALUES ('".$_GET['username']."');" );
     print_r( $result );
