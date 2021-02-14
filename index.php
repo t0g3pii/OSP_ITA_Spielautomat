202670427ae7ab2d53fd9783d83ba55b782d33d6
t0g3pii aka Nico <49153387+t0g3pii@users.noreply.github.com>
2021-02-14 13:40:34 +0100

    lel

--- index.php
+++ index.php
@@ -18,8 +18,7 @@
 </head>
 
 <body>
-    <div class="modal show" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal show" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-modal="true" role="dialog" style="display: block;">
         <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
@@ -30,7 +29,7 @@
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">@</span>
                             <input type="text" class="form-control" placeholder="Benutzername" name="username" minlength="4" maxlength="16" required>
-                            <input type="password" pattern="[0-9]*" inputmode="numeric" class="form-control" placeholder="4-Stelliger PIN" name="pin" minlength="4" maxlength="4" required>
+                            <input type="password" pattern="[0-9]*" inputmode="numeric" class="form-control" placeholder="4-Stelliger Zahlen PIN" name="pin" minlength="4" maxlength="4" required>
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -41,11 +40,6 @@
             </div>
         </div>
     </div>
-    <script>
-        $(window).load(function () {
-            $('#myModal').modal('show');
-        });
-    </script>
 </body>
 
 </html>
