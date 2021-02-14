18da895d8c8c5c55628ce59b6bf175ba0646b8bd
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 13:35:35 +0100

    Create game.php

---
+++ game.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+if (!isset($_SESSION["UUID"])) {
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $extra = 'index.php';
+    header("Location: http://$host$uri/$extra");
+    session_destroy();
+    exit();
+}
+
+echo $_SESSION["UUID"];
