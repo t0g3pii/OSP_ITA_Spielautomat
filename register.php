d1b5cc5edd4bc87435ea652c9216f136ad855be0
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2021-02-14 12:55:30 +0100

    Update register.php

--- register.php
+++ register.php
@@ -2,6 +2,9 @@
     require("inc/db.inc.php");
     $st = $pdo->prepare( "INSERT INTO `user` (`username`) VALUES ('".$_GET['username']."');" );
     if (!$st->execute()) {
-        print_r($st->errorInfo());
+        $host  = $_SERVER['HTTP_HOST'];
+        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        $extra = 'index.php?error=username';
+        header("Location: http://$host$uri/$extra");
     }
 ?>
